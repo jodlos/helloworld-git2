ef7d1547234986386e2577d9e5d4c1f3f99ceb00
bjodlos <barbara.jodlos@gmail.com>
2024-07-05 19:36:12 +0200

    Drugi commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World Improvements!");
         System.out.println("Zmieniony kod");
+        System.out.println("Nowe zmiany. Drugi commit.");
     }
 
 }
