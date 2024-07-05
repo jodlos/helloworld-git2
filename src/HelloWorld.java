adebbe17bb5c8b8811fdc3f984a2b540376a6519
bjodlos <barbara.jodlos@gmail.com>
2024-07-05 19:38:07 +0200

    Revert "Drugi commit"
    This reverts commit ef7d1547234986386e2577d9e5d4c1f3f99ceb00.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World Improvements!");
         System.out.println("Zmieniony kod");
-        System.out.println("Nowe zmiany. Drugi commit.");
     }
 
 }
