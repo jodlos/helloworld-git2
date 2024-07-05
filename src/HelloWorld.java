549ccfd7f9783c5da5391bff5214f4a44d36a4dd
bjodlos <barbara.jodlos@gmail.com>
2024-07-05 19:33:38 +0200

    Zmieniony kod

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World Improvements!");
+        System.out.println("Zmieniony kod");
     }
+
 }
